39c614c5bb2619b48d50612475ba5792f858f934
Phillip Ho <philasan@gmail.com>
2021-03-16 13:32:32 -0700

    use case assumptions

--- mysql_schema.sql
+++ mysql_schema.sql
@@ -4,8 +4,10 @@ CREATE DATABASE questionanswers;
 
 use questionanswers;
 
+-- 1076b per row
 CREATE TABLE questions (
   question_id INT NOT NULL AUTO_INCREMENT,
+  product_id INT NOT NULL,
   question_body TEXT(1000),
   question_date DATETIME,
   asker_name VARCHAR(60),
@@ -16,6 +18,7 @@ CREATE TABLE questions (
   PRIMARY KEY (question_id)
 );
 
+-- 1076b per row
 CREATE TABLE answers (
   id INT NOT NULL AUTO_INCREMENT,
   question_id INT NOT NULL,
@@ -23,18 +26,43 @@ CREATE TABLE answers (
   answer_date DATETIME,
   answerer_name VARCHAR(60),
   helpfulness SMALLINT UNSIGNED,
+  reported BOOLEAN,
 
   UNIQUE KEY (id),
   PRIMARY KEY (id),
   FOREIGN KEY (question_id) REFERENCES questions (question_id)
 );
 
+-- 263 bytes per row
 CREATE TABLE photos (
   id INT NOT NULL AUTO_INCREMENT,
   answer_id INT NOT NULL,
-  url VARCHAR(300),
+  url VARCHAR(255),
 
   UNIQUE KEY (id),
   PRIMARY KEY (id),
   FOREIGN KEY (answer_id) REFERENCES answers (id)
 );
+
+/*
+10k products
+
+on amazon it seemed like reviews and questions were left at a rate of 3:1
+If we were to assume all products were modestly popular with 3000 reviews, we could expect...
+1000 questions per product = 10,000,000 questions
+
+Amazon gets so many questions that they only show the ones with answers but even then each question only gets below 10 answers for the most part.
+10 answers per question = 10,000,000 answers
+
+I have no good indicator for how many photos are left for answers so assuming people are very active...
+3 photos per answer = 30,000,000 photos
+
+
+high use case:
+10,000,000 * 1076 bytes = 10GB
+10,000,000 * 1076 bytes = 10GB
+30,000,000 * 263 bytes = 7.89 GB
+
+** will re-review once I see data
+
+*/
